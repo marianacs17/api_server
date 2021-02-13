1ef09e48f0e60a5f744493c2eb8b5c945836f0e1
Mariana <mariana.csahagun@hotmail.com>
2021-02-12 20:44:39 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,52 @@
+//Importing packages
+const express = require('express');
+const morgan = require('morgan');
+const axios = require('axios');
+
+//Using packages
+const app = express();
+
+//Middleware to read json objects
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Routes to access the root directory of the api
+app.get('/', (req, res) => {
+    res.send("<h1>Pretty Mariana</h1>");
+})
+
+//Route to test a json response
+app.get('/test', (req, res) => {
+    res.json({
+        prop1: "hello",
+        prop2: "world"
+    })
+})
+
+//POST routes
+app.post('/test_post',(req,res)=>{
+    console.log(req.body.prop1);
+    aux = req.body.prop1
+    //Answer to the client
+    res.send(`Received: ${aux},${req.body.prop2}`);
+})
+
+//Testing for Axios APIs
+//Using parameters
+app.get('/rick/:id',(req,res)=> {
+    const URL = `https://rickandmortyapi.com/api/character/ ${req.params.id}`
+    axios.get(URL)
+        .then(function(response){
+            console.log(response.data)
+            res.send(response.data)
+        })
+        .catch(function(error){
+            console.log(error);
+            res.send(error);
+        });
+})
+
+//Listen Server
+app.listen(4000, () =>{
+    console.log("Server running on port 4000");
+})
